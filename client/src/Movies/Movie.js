18e4229c8a9d27a1b43abdfeafc906c86783a164
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-20 12:04:53 -0700

    Add functionality to savedList & check for duplicates in saved list

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -61,7 +61,7 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={_ => this.props.addToSavedList(this.state.movie)}>Save</div>
       </div>
     );
   }
